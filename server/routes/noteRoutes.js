46e20e4664b5443a7c5e783b4f9feff7aefdc505
Idrees Mushtaq <parrayidrees198@gmail.com>
2025-12-13 15:25:04 +0530

    feat:Added  pinned/tags/category/image fields to Note model

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
+const upload = require("../middleware/upload");
 
 
 const {
@@ -8,6 +9,10 @@ const {
     getNotes,
     updateNote,
     deleteNote,
+    togglePin,
+    toggleFavorite,
+    uploadImage,
+
 } = require('../controllers/noteControllers'); 
 
 // Correct routes
@@ -15,5 +20,8 @@ router.get("/", protect, getNotes);
 router.post("/", protect, createNote);
 router.put("/:id", protect, updateNote);
 router.delete("/:id", protect, deleteNote);
+router.put("/:id/togglePin", protect, togglePin);
+router.put("/:id/toggleFavorite", protect, toggleFavorite);
+router.post("/:id/upload", protect, upload.single('image'), uploadImage);  
 
 module.exports = router;
